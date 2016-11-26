0d744dfb2fb420a96496bf99e221e2cb6db747ba
Tony Bowden <tony@tmtm.com>
2016-11-26 10:36:43 +0000

    Replace WDQ with SPARQL

--- scraper.rb
+++ scraper.rb
@@ -4,6 +4,9 @@
 require 'wikidata/fetcher'
 
 names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/jordan-house-of-representatives-wikipedia', column: 'wikiname')
-ids = EveryPolitician::Wikidata.wdq('claim[39:17524418]')
 
-EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { en: names }, output: false)
+sparq = 'SELECT ?item WHERE { ?item wdt:P39 wd:Q%s . }'
+ids = EveryPolitician::Wikidata.sparql(sparq % 17524418)
+raise "No ids" if ids.empty?
+
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { en: names })
